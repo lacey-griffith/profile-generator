3bfa59e02e0717e2b9c686a5c507eefcbe371fb5
Lacey <lacey.griffith@g.austincc.edu>
2021-04-07 09:23:54 -0500

    add page-template

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,5 @@
-
+const generatePage = (name, github) => {
+    return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -8,8 +9,10 @@
         <title>Portfolio Demo</title>
     </head>
     <body>
-    <h1>Lacey</h1>
-    <h2><a href="https://github.com/lacey-griffith">Github</a></h2>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
     </body>
     </html>
-    
+    `;
+};
+module.exports = generatePage;
